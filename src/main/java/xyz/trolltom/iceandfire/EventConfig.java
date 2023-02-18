5716fe837961c050d7e9c284f3f1fbcb860f6723
SY573M404 <admin@trolltom.xyz>
2023-02-18 17:33:26 +0500

    Add event config boilerplate

---
+++ src/main/java/xyz/trolltom/iceandfire/EventConfig.java
@@ -0,0 +1,18 @@
+package xyz.trolltom.iceandfire;
+
+import com.gamerforea.eventhelper.config.Config;
+import com.gamerforea.eventhelper.config.ConfigBoolean;
+import com.gamerforea.eventhelper.config.ConfigUtils;
+
+@Config(name = "IceAndFire")
+public class EventConfig {
+    private static final String CATEGORY_GRIEFING = "griefing";
+
+    @ConfigBoolean(name = "griefRegions", category = CATEGORY_GRIEFING)
+    public static boolean griefRegions = false;
+
+    public static void init()
+    {
+        ConfigUtils.readConfig(EventConfig.class);
+    }
+}
